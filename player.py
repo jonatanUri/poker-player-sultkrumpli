51bb32117095888f440d2a5e19ccac2d4c1c4534
Jonatan Uri <urijoni@gmail.com>
2019-05-20 11:27:33 +0200

    all-in

--- player.py
+++ player.py
@@ -4,10 +4,33 @@ class Player:
 
     def betRequest(self, game_state):
 
+        myStack = game_state["players"]["in_action"]["stack"]
+        simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
+
         return game_state["players"]["in_action"]["stack"]
 
     def showdown(self, game_state):
         pass
 
 
+def simpleRaise(current_buy_in, bet, minimum_raise):
+    return current_buy_in - bet + minimum_raise
 
+
+def getCardsInPlay(game_state):
+    hand = game_state["players"]["in_action"]["hole_cards"]
+    table = game_state["community_cards"]
+    all = []
+    for card in hand:
+        all.append(card)
+    for card in table:
+        all.append(card)
+
+
+
+cardValues = dict(
+    J=11,
+    Q=12,
+    K=13,
+    A=14
+)
