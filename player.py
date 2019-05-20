977dac475236fbb9f07787dbf5dc6e946ba797f7
Jonatan Uri <urijoni@gmail.com>
2019-05-20 10:39:49 +0200

    hm

--- player.py
+++ player.py
@@ -3,8 +3,11 @@ class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
-        return 0
+
+        return game_state["minimum_raise"]
 
     def showdown(self, game_state):
         pass
+
+
 
