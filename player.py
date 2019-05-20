7046551ae162990ccc5309a6399767ff14f9e54a
Jonatan Uri <urijoni@gmail.com>
2019-05-20 11:33:46 +0200

    all-in if pair in hand and suit match in hand

--- player.py
+++ player.py
@@ -7,7 +7,16 @@ class Player:
         myStack = game_state["players"]["in_action"]["stack"]
         simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
 
-        return game_state["players"]["in_action"]["stack"]
+        cardsInPlay = getCardsInPlay(game_state)
+        cardsInHand = getCardsInHand(game_state)
+
+        if cardsInHand[0]["rank"] == cardsInHand[1]["rank"]:
+            return game_state["players"]["in_action"]["stack"]
+
+        if cardsInHand[0]["suit"] == cardsInHand[1]["suit"]:
+            return simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
+
+        return 0
 
     def showdown(self, game_state):
         pass
@@ -25,8 +34,10 @@ def getCardsInPlay(game_state):
         all.append(card)
     for card in table:
         all.append(card)
+    return all
 
-
+def getCardsInHand(game_state):
+    return game_state["players"]["in_action"]["hole_cards"]
 
 cardValues = dict(
     J=11,
