930125e1599278b3dbf7b8db1a017b363f87cb67
Jonatan Uri <urijoni@gmail.com>
2019-05-20 11:52:54 +0200

    back to minimum raise on default

--- player.py
+++ player.py
@@ -1,22 +1,24 @@
 
 class Player:
-    VERSION = "1.3"
+    VERSION = "1.4"
 
     def betRequest(self, game_state):
 
-        myStack = game_state["players"]["in_action"]["stack"]
+        AllIn = game_state["players"]["in_action"]["stack"]
         simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
 
         cardsInPlay = getCardsInPlay(game_state)
         cardsInHand = getCardsInHand(game_state)
 
-        if cardsInHand[0]["rank"] == cardsInHand[1]["rank"]:
-            return game_state["players"]["in_action"]["stack"]
+        bet_size = game_state["minimum_raise"]
 
         if cardsInHand[0]["suit"] == cardsInHand[1]["suit"]:
-            return simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
+            bet_size = simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
 
-        return game_state["minimum_raise"]
+        if cardsInHand[0]["rank"] == cardsInHand[1]["rank"]:
+            bet_size = AllIn
+
+        return bet_size
 
     def showdown(self, game_state):
         pass
