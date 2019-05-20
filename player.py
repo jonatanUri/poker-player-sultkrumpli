5dcfe4c776dcf22d90f4c17d9b90f338d4e62da3
Jonatan Uri <urijoni@gmail.com>
2019-05-20 13:45:21 +0200

    tried to fix error2

--- player.py
+++ player.py
@@ -3,32 +3,39 @@ class Player:
     VERSION = "1.7"
 
     def betRequest(self, game_state):
+        try:
+            AllIn = game_state["players"]["in_action"]["stack"]
+            simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
 
-        AllIn = game_state["players"]["in_action"]["stack"]
-        simpleRaise(game_state["current_buy_in"], game_state["players"]["in_action"]["bet"], game_state["minimum_raise"])
+            cardsInPlay = getCardsInPlay(game_state)
+            cardsInHand = getCardsInHand(game_state)
+        except:
+            print("---error1")
 
-        cardsInPlay = getCardsInPlay(game_state)
-        cardsInHand = getCardsInHand(game_state)
-
-        bet_size = 0
+        bet_size = 10
         minimum_raise = game_state["current_buy_in"] + game_state["minimum_raise"]
 
+        try:
+            if cardsInHand[0]["rank"] == 7 and cardsInHand[1]["rank"] == 2:
+                return 0
+            elif cardsInHand[0]["rank"] == 2 and cardsInHand[1]["rank"] == 7:
+                return 0
+        except:
+            print("---error2")
 
+        try:
+            if cardsInHand[0]["rank"] + cardsInHand[1]["rank"] > 18:
+                bet_size = minimum_raise
+            if cardsInHand[0]["rank"] + cardsInHand[1]["rank"] > 20:
+                bet_size += minimum_raise
+            if cardsInHand[0]["suit"] == cardsInHand[1]["suit"]:
+                # bet_size = bet_size
+                pass
 
-        if cardsInHand[0]["rank"] == 7 and cardsInHand[1]["rank"] == 2:
-            return 0
-        elif cardsInHand[0]["rank"] == 2 and cardsInHand[1]["rank"] == 7:
-            return 0
-
-        if cardsInHand[0]["rank"] + cardsInHand[1]["rank"] > 18:
-            bet_size = minimum_raise
-        if cardsInHand[0]["rank"] + cardsInHand[1]["rank"] > 20:
-            bet_size += minimum_raise
-        if cardsInHand[0]["suit"] == cardsInHand[1]["suit"]:
-            bet_size = bet_size
-
-        if cardsInHand[0]["rank"] == cardsInHand[1]["rank"]:
-            bet_size += minimum_raise
+            if cardsInHand[0]["rank"] == cardsInHand[1]["rank"]:
+                bet_size += minimum_raise
+        except:
+            print("---error3")
 
         return bet_size
 
